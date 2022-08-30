b0546a05a40d08cf5ecf29ab6b35432b2cea8384
Nithin M.K <nithinmkurien@gmail.com>
2022-08-30 11:09:36 +0530

    02-frontend-synchronizing-post-additions

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -40,14 +40,21 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080');
+    const socket = openSocket('http://localhost:8080');
+    socket.on('posts', data => {
+      if (data.action === 'create') {
+        this.addPost(data.post);
+      }
+    });
   }
 
   addPost = post => {
     this.setState(prevState => {
       const updatedPosts = [...prevState.posts];
       if (prevState.postPage === 1) {
-        updatedPosts.pop();
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
         updatedPosts.unshift(post);
       }
       return {
@@ -183,8 +190,6 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
           }
           return {
             posts: updatedPosts,
